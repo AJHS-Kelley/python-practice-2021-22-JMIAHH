240420eee28ca1e62edf7aef2768dbe07e89761b
Jeremiah H <90330037+JMIAHH@users.noreply.github.com>
2021-10-29 09:37:59 -0400

    [1A] Version 1
    Created 3 variables:
    Integers
    Float
    String

---
+++ .github/variables_practice.py
@@ -0,0 +1,9 @@
+# Variable Practice , Jeremiah Harrell, 10/29/2021, 09:26am, Verison 0.1
+
+# Three Main Types of Variables for this class
+
+health = 345 #INTEGERS, any +/- whole number AND 0.
+account_balance = 12345.52 # FLOATING POINT, any +/- with decimal.
+user_name = "Thomas Edison" # STRING, letters/numbers printed on screen.
+
+print(health)
